1e3f42a42e504fa086b10f75376ba0353bd44f02
pmutunga <paskwa.mutunga@ipsafari.com>
2019-04-11 20:59:54 -0400

    Paskwa Auth Client folders

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
+

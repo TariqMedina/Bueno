fa9dcc4d81f118ae9e82991724c6ff5be5185999
Verde <a.green.designer@gmail.com>
2019-04-16 20:23:54 -0400

    further updates to chat logic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import io from 'socket.io-client';
+import {USER_CONNECTED, LOGOUT} from '../Events';
+import LoginForm from './LoginForm';
 
 const socketUrl = "http://localhost:3231"
 export default class Layout extends Component {
@@ -7,7 +9,8 @@ export default class Layout extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            socket: null
+            socket: null,
+            user: null
         };
     }
 
@@ -20,14 +23,27 @@ export default class Layout extends Component {
         socket.on('connect', () => {
             console.log("Connected");
         })
-        this.setState({ socket });
+        this.setState({socket});
+    }
+
+    setUser = (user) => {
+        const {socket} = this.state
+        socket.emit(USER_CONNECTED, user);
+        this.setState({user});
+    }
+
+    logout = () => {
+        const {socket} = this.state
+        socket.emit(LOGOUT);
+        this.setState({user: null});
     }
 
     render() {
-        const { title } = this.props;
+        const {title} = this.props;
+        const {socket} = this.state;
         return (
             <div className="container">
-                {title}
+                <LoginForm socket={socket} setUser={this.setUser}/>
             </div>
         );
     }
--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -1,5 +1,38 @@
 const io = require('./index.js').io;
 
+const {VERIFY_USER, USER_CONNECTED, LOGOUT} = require('../Events');
+
+const {createUser, createMessage, createChat} = require('../Factories');
+
+const connectedUser = { };
+
 module.exports = function(socket){
     console.log("Socket ID" + socket.id);
+
+    socket.on(VERIFY_USER, (nickname, callback) => {
+        if(isUser(connectedUser, nickname)){
+            callback({isUser: true, user: null})
+        }
+        else{
+            callback({isUser: false, user: createUser({name: nickname})})
+        }
+    })
+}
+
+
+
+function addUser(userList, user){
+    let newList = Object.assign({}, userList)
+    newList[user.name] = user
+    return newList
+}
+
+function removeUser(userList, username){
+    let newList = Object.assign({}, userList)
+    delete newList[username]
+    return newList
+}
+
+function isUser(userList, username){
+    return username in userList
 }

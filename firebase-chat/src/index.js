f94f8e038675dbb28d477a84c7cf6b2daa913493
Verde <a.green.designer@gmail.com>
2019-04-22 20:48:27 -0400

    Chat functioning

--- firebase-chat/src/index.js
+++ firebase-chat/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '../src/components/App/App';
 import * as serviceWorker from './serviceWorker';
+
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

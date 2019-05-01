7e1356d81457f8d30d1f64b434900a72e126e5f8
Jessen <jessendasilva1@gmail.com>
2019-04-30 22:38:53 -0400

    firebase max users logic

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,55 +1,140 @@
-import React, { Component } from "react";
-import "./style.css";
-import firebase from "firebase";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import image from "./bueno-logo.png";
+import React, { Component } from 'react';
+import './style.css';
+import firebase from 'firebase';
+import { Link } from "react-router-dom";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import image from './bueno-logo.png';
 
 class Jumbotron extends Component {
-  state = { isSignedIn: false };
-uiConfig = {
-  signInFlow: "popup",
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  callbacks: {
-    signInSuccess: () => false
+  state = {
+    user: null,
+    isSignedIn: false,
+    firebasePlayers: "",
+    activePlayers: [],
+    gameMessage: 'Join the Game!',
+    gameBtnClass: 'btn btn-primary btn-lg join-btn',
+    disabled: false,
+    redirect: false
+  };
+
+  uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+
+  componentDidMount() {
+    let firebasePlayerListCount;
+    firebase.database().ref('/players').on('value', (snapshot) => {
+      if(snapshot.val() === null){
+        firebasePlayerListCount = 0;
+      } else {
+        firebasePlayerListCount = Object.keys(snapshot.val()).length;
+      }
+      //console.log(firebasePlayerListCount);
+      this.setState({
+        firebasePlayers: firebasePlayerListCount
+      }, () => {
+        if(this.state.firebasePlayers === 4){
+          this.setState({
+            gameMessage: 'Sorry, we have 4 players. Please wait',
+            gameBtnClass: 'btn btn-info btn-lg join-btn',
+            disabled: true
+          })
+        }
+      })
+    })
+    //this.setState({})
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user });
+    });
   }
-};
 
-componentDidMount() {
-  firebase.auth().onAuthStateChanged(user => {
-    this.setState({ isSignedIn: !!user });
-  });
-}
+  handleClick = () => {
+    if (this.state.firebasePlayers <= 4) {
+      console.log('hello less than 4');
+      this.setState({
+        activePlayers: [
+          ...this.state.activePlayers,
+          firebase.auth().currentUser.displayName
+        ]
+      });
+      //push current user to firebase
+      const playersRef = firebase.database().ref('players');
+      const player = {
+        user: firebase.auth().currentUser.email,
+        name: firebase.auth().currentUser.displayName
+      };
+      playersRef.push(player);
+    } else {
+      console.log('hello greater than 4');
+      this.setState({
+        gameMessage: 'Sorry, we have 4 players. Please wait',
+        gameBtnClass: 'btn btn-info btn-lg join-btn',
+        disabled: true
+      });
+      console.log('sorry we have enough players');
+    }
+  };
 
-render() {
-  return (
-    <div className="jumbotron text-center">
-      <img className="bueno-logo" src={image}></img>
-      <p className="intro-text">
-      Spice up your life with Bueno, a digital version of Uno, the popular card game!
-      </p>
-      {this.state.isSignedIn ? (
-              <div>
-              
-              <h2>Welcome to Bueno, {firebase.auth().currentUser.displayName}!</h2>
-              <br></br>
-              <br></br>
-                  <a className="btn btn-primary btn-lg" href="/game" role="button">Join the Game!</a>
-              </div>
-              
-            ) : (
-      // <a className="btn btn-primary btn-lg" href="/login" role="button">Log in to play!</a>
-      <span>
-                  <StyledFirebaseAuth
-                    uiConfig={this.uiConfig}
-                    firebaseAuth={firebase.auth()}
-                  />
-                </span>
-            )}
-    </div>
+  // assign players
+  //++++++
+
+  assignPlayers = () => {
+    console.log('player assignment begins');
+    //I need to know the username from handleClick
+    // let userId = '';
+    // userId = firebase.auth().currentUser.displayName;
+
+    // let gameRef = new Firebase(GAME_LOCATION);
+  };
+  // ++++
+
+  render() {
+    return (
+      <div className="jumbotron text-center">
+        <img className="bueno-logo" src={image} />
+        <p className="intro-text">
+          Spice up your life with Bueno, a digital version of Uno, the popular
+          card game!
+        </p>
+        {this.state.isSignedIn ? (
+          <div>
+            <h2>
+              Welcome to Bueno, {firebase.auth().currentUser.displayName}!
+            </h2>
+            <br />
+            <br />
+            {/* <a className="btn btn-primary btn-lg" href="/game" role="button">
+              Join the Game!
+            </a> */}
+            {/*<Link to="/game">*/}
+              <button
+                className={this.state.gameBtnClass}
+                onClick={this.handleClick}
+                role="button"
+                disabled={this.state.disabled ? true : false}
+              >
+                {this.state.gameMessage}
+              </button>
+          {/*</Link>*/}
+          </div>
+        ) : (
+          // <a className="btn btn-primary btn-lg" href="/login" role="button">Log in to play!</a>
+          <span>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </span>
+        )}
+      </div>
     );
   }
 }

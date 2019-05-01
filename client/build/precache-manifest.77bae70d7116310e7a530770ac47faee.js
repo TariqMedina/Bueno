48709230eb79a3a97098d5f14a42ed43b1c535f6
Tariq M <tariqmedina@gmail.com>
2019-04-30 21:10:49 -0400

    Testing

---
+++ client/build/precache-manifest.77bae70d7116310e7a530770ac47faee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c63e9a19d694e2328007ff58e37d8d49",
+    "url": "/static/media/card-yellow-reverse.c63e9a19.png"
+  },
+  {
+    "revision": "782839d1111302fad753534d0c511601",
+    "url": "/static/media/card-red-reverse.782839d1.png"
+  },
+  {
+    "revision": "575652134481db164d12202b48d3cfd2",
+    "url": "/static/media/card-green-reverse.57565213.png"
+  },
+  {
+    "revision": "7f33ab775f9de0e86dd27cfad36862d4",
+    "url": "/static/media/card-blue-reverse.7f33ab77.png"
+  },
+  {
+    "revision": "d39f2b111d169d3f6552262800c40da9",
+    "url": "/static/media/card-back.d39f2b11.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9dbe6464fa7cb6d70bce",
+    "url": "/static/js/main.c722ab67.chunk.js"
+  },
+  {
+    "revision": "616cd9b3ca7d57540279",
+    "url": "/static/js/2.8ee5e19e.chunk.js"
+  },
+  {
+    "revision": "9dbe6464fa7cb6d70bce",
+    "url": "/static/css/main.0f4207e9.chunk.css"
+  },
+  {
+    "revision": "fcab02577d8d709cd3b7734716fd0a30",
+    "url": "/index.html"
+  }
+];
